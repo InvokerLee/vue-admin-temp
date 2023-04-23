d828f8e20cee2f5dfe20c4e025df9ab7ae433ad0
szjls <demonslth@outlook.com>
2023-04-23 21:00:19 +0800

    1

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,88 +1,116 @@
 <template>
   <div>
-    <div>dashboard</div>
-
-    <BaseTable
-      autoHeight
-      :loading="tableData.loading"
-      :column="tableData.column"
-      :data="tableData.data"
-    >
-    </BaseTable>
-    <!--
-      pagination
-      :current-page.sync="params.page"
-      :page-size="params.limit"
-      :total="tableData.total"
-      @size-change="pageSizeChange"
-      @current-change="pageChange"
-    -->
+    <div>
+      <el-radio-group v-model="radio1">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio2">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio3">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+    </div>
   </div>
 </template>
 
 <script>
+import SkuModule from './lib';
+
+const specGroups = [
+  {
+    id: 1,
+    name: '款式',
+    specs: [
+      { id: 11, name: '树莓红茶' },
+      { id: 12, name: '樱花金' },
+      { id: 13, name: '柠檬海盐' },
+      { id: 14, name: '白夜霓虹' },
+    ],
+  },
+  {
+    id: 2,
+    name: '长度',
+    specs: [
+      { id: 21, name: '39cm' },
+      { id: 22, name: '43cm' },
+      { id: 23, name: '45cm' },
+      { id: 24, name: '48cm' },
+    ],
+  },
+  {
+    id: 3,
+    name: '尺码',
+    specs: [
+      { id: 31, name: 'S' },
+      { id: 32, name: 'M' },
+      { id: 33, name: 'L' },
+    ],
+  },
+];
+const products = [
+  {
+    id: '1',
+    specs: [
+      { id: 12, name: '樱花金' },
+      { id: 21, name: '39cm' },
+      { id: 31, name: 'S' },
+    ],
+  },
+  {
+    id: '2',
+    specs: [
+      { id: 12, name: '樱花金' },
+      { id: 21, name: '39cm' },
+      { id: 32, name: 'M' },
+    ],
+  },
+  {
+    id: '3',
+    specs: [
+      { id: 12, name: '樱花金' },
+      { id: 22, name: '43cm' },
+      { id: 32, name: 'M' },
+    ],
+  },
+  {
+    id: '4',
+    specs: [
+      { id: 11, name: '树莓红茶' },
+      { id: 22, name: '43cm' },
+      { id: 33, name: 'L' },
+    ],
+  },
+];
+
 export default {
   data() {
     return {
-      params: {
-        limit: 20,
-        page: 1,
-      },
-      tableData: {
-        loading: false,
-        data: [
-          { aa: 1, bb: 1 },
-          { aa: 2, bb: 2 },
-          { aa: 3, bb: 3 },
-        ],
-        column: [
-          {
-            width: 55,
-            type: 'selection',
-          },
-          {
-            width: 55,
-            label: '序号',
-            type: 'index',
-          },
-          {
-            prop: 'aa',
-            label: '厂商型号',
-          },
-          {
-            prop: 'bb',
-            label: '厂商/品牌',
-            renderHeader: (h, scope) => {
-              return <span class='font-20'>{ scope.column.label }</span>;
-            },
-            render: (h, scope) => {
-              return <span class='font-20'>{ scope.row.bb }</span>;
-            },
-          },
-        ],
-        total: 100,
-      },
+      groups: specGroups,
+      usefulCombinations: products,
+
+      radio1: null,
+      radio2: null,
+      radio3: null,
     };
   },
   created() {
-    // this.$dialog({
-    //   props: {
-    //     title: '修改预计发货时间',
-    //     width: '480px',
-    //   },
-    //   component: updateExpectDate,
-    //   componentProps: { defaultData },
-    //   on: {
-    //     success: () => {
-    //       this.getList();
-    //     },
-    //   },
-    // });
+    new SkuModule(this.groups, this.usefulCombinations);
   },
   methods: {
-    selectionChange(val) {
-      // console.log(val);
-    },
   },
 };
 </script>
